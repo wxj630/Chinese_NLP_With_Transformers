c5e94019a9b84b54afcecd0cc07847009eda93c6
IDEA <wuxiaojun@idea.edu.cn>
2021-07-27 20:13:47 +0800

    add summarization.ipynb

--- main.py
+++ main.py
@@ -14,3 +14,7 @@ if __name__ == '__main__':
     print_hi('PyCharm')
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
+
+from transformers import AutoModelForSeq2SeqLM, DataCollatorForSeq2Seq, Seq2SeqTrainingArguments, Seq2SeqTrainer
+
+
